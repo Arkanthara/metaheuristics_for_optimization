157198dfd6647d9995af2ceede5a5a7aa5c75264
Vango Romano <tobie@lolness>
2025-10-20 16:36:27 +0200

    Add parallel tempering

--- code/tp2.py
+++ code/tp2.py
@@ -1,63 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
-import random
 
 
 class SA:
     def __init__(self, cities: np.ndarray) -> None:
         self.cities = cities
         self.num_cities = cities.shape[0]
-        # self.swap_matrix = self._build_swap_matrix()
 
     def _cost(self, path: np.ndarray) -> float:
         assert len(path.shape) == 1, f"Path must be 1D ! Actual: {path.shape}"
         shift = np.roll(path, 1)
         return np.linalg.norm(self.cities[path] - self.cities[shift], axis=1).sum()
-
-    # def _cost(self, paths: np.ndarray) -> np.ndarray:
-    #     if len(paths.shape) != 2:
-    #         paths = np.array([paths])
-    #     assert len(paths.shape) == 2, (
-    #         f"path must be list of paths, so 2D ! Actual path_shape: {paths.shape}"
-    #     )
-    #     shift = np.roll(paths, 1, axis=1)
-    #     return np.linalg.norm(self.cities[paths] - self.cities[shift], axis=2).sum(
-    #         axis=1
-    #     )
-    #
-    # def _canonical(self, path: np.ndarray) -> tuple:
-    #     """
-    #     Function that put a path on canonical form.
-    #     It means that all the variantions of the same path will be written in the same way.
-    #     For that, working with tuple allows to use comparison between lists.
-    #
-    #     Parameters
-    #     ----------
-    #     path : np.ndarray
-    #         Path to compute canonical form
-    #
-    #     Returns
-    #     -------
-    #     tuple
-    #         Canonical form of the path
-    #
-    #     """
-    #     same = [tuple(np.roll(path, i)) for i in range(len(path))]
-    #     same += [tuple(np.roll(path[::-1], i)) for i in range(len(path))]
-    #     return min(same)
-
-    # def _build_swap_matrix(self) -> np.ndarray:
-    #     n = self.num_cities
-    #     init = np.arange(n).astype(int)
-    #     matrix = []
-    #     canonical_form = self._canonical(init)
-    #     for i in range(n - 1):
-    #         for j in range(i + 1, n):
-    #             new_permutation = self._canonical(self._swap(init, i, j))
-    #             if new_permutation not in matrix and new_permutation != canonical_form:
-    #                 matrix.append(new_permutation)
-    #     return np.array(matrix).astype(int)
 
     def _swap(self, path: np.ndarray, i: int, j: int) -> np.ndarray:
         swap_path = path.copy()
@@ -68,15 +22,15 @@ class SA:
         i, j = np.random.choice(len(path), size=2)
         return self._swap(path, i, j)
 
-    def greedy(self) -> np.ndarray:
+    def greedy(self) -> tuple:
         path = [np.random.choice(np.arange(self.num_cities).astype(int))]
         for i in range(self.num_cities - 1):
             distance = np.linalg.norm(self.cities - self.cities[path[i]], axis=1)
             distance[path] = np.inf
             path.append(np.argmin(distance))
-        return np.array(path)
+        return np.array(path), self._cost(np.array(path))
 
-    def simulated_annealing(self) -> np.ndarray:
+    def simulated_annealing(self) -> tuple:
         path = np.random.permutation(self.num_cities)
         current = path
         delta = []
@@ -86,16 +40,18 @@ class SA:
         cost_improvment = False
         best_cost = self._cost(current)
         best_path = path
+        iter = 0
+        T_0 = 0
 
-        # Init temperature
-        for i in range(100):
+        # Initialize temperature
+        for _ in range(100):
             new_path = self.permute(current)
             cost = self._cost(new_path)
             # index = np.argmin(cost)
             delta.append(cost - self._cost(current))
             current = new_path
         T = -np.mean(np.abs(delta)) / np.log(0.5)
-        print(f"Initial temperature T_0: {T}")
+        T_0 = T
         delta = 0
         current = path
 
@@ -112,7 +68,7 @@ class SA:
                 accepted = 0
                 T *= 0.9
 
-            # Choose neighbor
+            # Choose new path
             new_path = self.permute(current)
             delta = self._cost(new_path) - self._cost(current)
             if delta <= 0:
@@ -120,42 +76,131 @@ class SA:
                 accepted += 1
 
             # Metropolis rule
-            else:
-                if np.random.rand() < np.exp(-delta / T):
-                    current = new_path
-                    accepted += 1
+            elif np.random.rand() < np.exp(-delta / T):
+                current = new_path
+                accepted += 1
+
             attempted += 1
 
             # Update best solution
             if self._cost(current) < best_cost:
-                # print(
-                #     f"current best fitness: {best_cost} and new best fitness: {self._cost(current)[0]}"
-                # )
                 cost_improvment = True
                 best_cost = self._cost(current)
                 best_path = current
 
-        return best_path
+            iter += 1
+
+        return best_path, iter, best_cost, T_0
+
+    def generate_T_factors(self, M: int = 4, type: str = "linear") -> np.ndarray:
+        x = np.arange(1, M + 1)
+
+        if type == "linear":
+            return x / x.max()
+        elif type == "quad":
+            quad = x**2
+            return quad / quad.max()
+        elif type == "exp":
+            exp = np.exp(x)
+            return exp / exp.max()
+        elif type == "log":
+            log = np.log(x)
+            return log / log.max()
+        else:
+            print(f"{type} is not a valid type ! Default is 'linear'")
+            return x / x.max()
+
+    def parallel_tempering(
+        self, max_iter: int = 10000, M: int = 4, type: str = "linear"
+    ) -> tuple:
+        paths = np.array([np.random.permutation(self.num_cities) for _ in range(M)])
+        current = paths[np.random.randint(len(paths))]
+        delta = []
+        best_cost = self._cost(current)
+        best_path = current
+        iter = 0
+        T_0 = 0
+
+        # Initialize temperature
+        for _ in range(100):
+            new_path = self.permute(current)
+            cost = self._cost(new_path)
+            # index = np.argmin(cost)
+            delta.append(cost - self._cost(current))
+            current = new_path
+        T = -np.mean(np.abs(delta)) / np.log(0.5) * self.generate_T_factors(M, type)
+        T_0 = T[-1]
+        delta = 0
+        cost = np.array([self._cost(path) for path in paths])
+
+        # Stopping condition
+        for i in range(max_iter):
+            for j in range(M):
+                # Choose new path
+                new_path = self.permute(paths[j])
+                delta = self._cost(new_path) - self._cost(paths[j])
+                if delta <= 0:
+                    paths[j] = new_path
+                    cost[j] = self._cost(new_path)
+
+                # Metropolis rule
+                elif np.random.rand() < np.exp(-delta / T[j]):
+                    paths[j] = new_path
+                    cost[j] = self._cost(new_path)
+
+            # Change temperature according to cost
+            index = np.argsort(cost, stable=True)
+            cost = cost[index]
+            paths = paths[index]
+
+            # Update best solution
+            if cost.min() < best_cost:
+                best_cost = cost.min()
+                best_path = paths[np.argmin(cost)]
+                iter = i
+
+        return best_path, iter, best_cost, T_0
 
     def graph(
         self,
-        path: np.ndarray = np.array([]),
-        isPath: bool = False,
+        path: bool = False,
         title: str = "Graph",
+        greedy: bool = False,
+        sa: bool = True,
+        max_iter: int = 10000,
+        M: int = 4,
+        type: str = "exp",
     ):
         x = self.cities[:, 0]
         y = self.cities[:, 1]
         plt.figure()
-        if isPath:
-            if path.size == 0:
-                print("You must provide a path if the option isPath is activated !")
-                exit(0)
-            path_x = self.cities[path, 0]
-            path_y = self.cities[path, 1]
-            plt.plot(path_x, path_y, color="green")
+        if path:
+            if greedy:
+                result, cost = self.greedy()
+                plt.plot([], [], " ", label=f"Fitness: {cost}")
+            elif sa:
+                result, iter, cost, T = self.simulated_annealing()
+                plt.plot(
+                    [],
+                    [],
+                    " ",
+                    label=f"Number of iterations: {iter}\nFitness: {cost}\n$T_0$: {T}",
+                )
+            else:
+                result, iter, cost, T = self.parallel_tempering(max_iter, M, type)
+                plt.plot(
+                    [],
+                    [],
+                    " ",
+                    label=f"Nb_iterations to find best fitness: {iter}\nFitness: {cost}\n$T_0$: {T}",
+                )
+            path_x = self.cities[result, 0]
+            path_y = self.cities[result, 1]
+            plt.plot(path_x, path_y, color="green", label="path")
             plt.plot([path_x[-1], path_x[0]], [path_y[-1], path_y[0]], color="green")
-        plt.plot(x, y, "ro")
+        plt.plot(x, y, "ro", label="cities")
         plt.title(title)
+        plt.legend()
         plt.show()
 
 
@@ -174,11 +219,45 @@ if __name__ == "__main__":
         "--benchmark",
         help="Benchmark problem with human trivial solution (vertex polygon)",
     )
+    parser.add_argument(
+        "-r",
+        "--random",
+        help="Random problem of n cities",
+    )
 
     # Parse arguments
     args = parser.parse_args()
 
     if args.benchmark:
         test = SA(benchmark(int(args.benchmark)))
-        test.graph(test.greedy(), isPath=True)
-        test.graph(test.simulated_annealing(), isPath=True)
+        test.graph(
+            path=True,
+            greedy=True,
+            title=f"Benchmark: greedy for {args.benchmark} cities",
+        )
+        test.graph(
+            path=True,
+            title=f"Benchmark: simulated annealing for {args.benchmark} cities",
+        )
+        test.graph(
+            path=True,
+            sa=False,
+            title=f"Benchmark: parallel tempering for {args.benchmark} cities",
+        )
+
+    if args.random:
+        test = SA(np.random.rand(args.random, 2))
+        test.graph(
+            path=True,
+            greedy=True,
+            title=f"Benchmark: greedy for {args.benchmark} cities",
+        )
+        test.graph(
+            path=True,
+            title=f"Benchmark: simulated annealing for {args.benchmark} cities",
+        )
+        test.graph(
+            path=True,
+            sa=False,
+            title=f"Benchmark: parallel tempering for {args.benchmark} cities",
+        )
